dfa607269640cfa5cfbf5d37adcd2409c3a5e2fd
Amisha Jangra <43825330+Mighty-Geek@users.noreply.github.com>
2020-07-21 23:21:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,39 +53,6 @@ function deleteCheck(e) {
         });
     }
 
-    //CHECK MARK
-    if (item.classList[0] === 'complete-btn') {
-        const todo = item.parentElement;
-        todo.classList.toggle("completed");
-    }
-}
-
-function filterTodo(e) {
-    const todos = todoList.childNodes;
-    todos.forEach(function (todo) {
-        switch (e.target.value) {
-            case "all":
-                todo.style.display = 'flex';
-                break;
-            case "completed":
-                if (todo.classList.contains('completed')) {
-                    todo.style.display = 'flex';
-                } else {
-                    todo.style.display = 'none';
-                }
-                break;
-            case "uncompleted":
-                if (!todo.classList.contains('completed')) {
-                    todo.style.display = 'flex';
-                } else {
-                    todo.style.display = 'none';
-                }
-                break;
-        }
-    });
-}
-
-
 function saveLocalTodos(todo) {
     // Check todos in local storage
     let todos;
